d10d54bef400684abd0bb3e9198468ae7ec6e53a
Baptiste Dupuis <dupuis.baptiste@gmail.com>
2023-06-09 07:36:11 +0200

    fetch data with useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,36 @@ import { useEffect, useState } from "react";
 import GetMovies from "./components/GetMovies";
 import { movies$ } from './movies.js';
 import { MovieContext } from "./main";
-import MovieRating from "./components/MovieRating";
-import MoviesFilter from "./components/MoviesFilter";
+import { useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const queryClient = new QueryClient();
+
+  
+
+  const { data, isLoading, isError } = useQuery('movies', async () => {
+    const response = await movies$;
+    return response;
+  });
 
   useEffect(() => {
-    const fetchData = async () => {
-      setMovies(await movies$);
-    };
-    fetchData();
-  }, []);
+    if (data) {
+      setMovies(data);
+    }
+  }, [data]);
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error while fetching data</p>;
+  } 
   const contextValue = {
     movies,
     setMovies,
@@ -26,6 +42,7 @@ function App() {
       <MovieContext.Provider value={contextValue}>
        <GetMovies />
       </MovieContext.Provider>
+      
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 
 
 export const MovieContext = React.createContext();
-
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>,)
